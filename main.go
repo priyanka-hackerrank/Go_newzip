0d34776eb844398f30c83f872f596386b952a336
priyanka-hackerrank <99803551+priyanka-hackerrank@users.noreply.github.com>
2024-08-21 20:41:59 +0530

    new zip

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello from GO!",
+		})
+	})
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	return r
+}
+
+func main() {
+	r := setupRouter()
+
+	r.Run()
+}
